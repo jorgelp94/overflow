7c458a0091350743058d5865b02c8a238ee01e65
Jorge Luis Perales <jorgeluis070294@hotmail.com>
2016-05-02 14:41:34 -0500

    accesos para arreglos en mv, ejemplos de fibonacci y factorial iterativos, caorrecciones

--- Entrega 6/maquina.py
+++ Entrega 6/maquina.py
@@ -17,50 +17,79 @@ def ejecutaMaquina(directorio, cuadruplos, constantes):
 	cont_cuadruplos = 0
 	retunValue = None
 
-	memoria_global = Memoria(nombre_programa, directorio[nombre_programa]['Variables Globales']['Memoria'])
-	memoria_activa = Memoria('main', directorio[nombre_programa]['Variables Locales']['Memoria'])
+	memoria_global = Memoria(nombre_programa, directorio[nombre_programa]['Variables Globales']['Memoria'], directorio[nombre_programa]['Temporales']['Memoria'])
+	memoria_activa = Memoria('main', directorio[nombre_programa]['Variables Locales']['Memoria'], directorio[nombre_programa]['Temporales']['Memoria'])
 	memoria_nueva = ""
 
-	print(memoria_global.name)
-	print(memoria_global.ints)
-	print(memoria_global.floats)
-	print(memoria_global.chars)
-	print(memoria_global.bools)
-	print(memoria_global.temp_int)
-	print(memoria_global.temp_float)
-	print(memoria_global.temp_char)
-	print(memoria_global.temp_bool)
-	print(memoria_activa.name)
-	print(memoria_activa.ints)
-	print(memoria_activa.floats)
-	print(memoria_activa.chars)
-	print(memoria_activa.bools)
-	print(memoria_activa.temp_int)
-	print(memoria_activa.temp_float)
-	print(memoria_activa.temp_char)
-	print(memoria_activa.temp_bool)
-	print("------------------------------------------------------------------------------------")
+	# print(memoria_global.name)
+	# print(memoria_global.ints)
+	# print(memoria_global.floats)
+	# print(memoria_global.chars)
+	# print(memoria_global.bools)
+	# print(memoria_global.temp_int)
+	# print(memoria_global.temp_float)
+	# print(memoria_global.temp_char)
+	# print(memoria_global.temp_bool)
+	# print(memoria_activa.name)
+	# print(memoria_activa.ints)
+	# print(memoria_activa.floats)
+	# print(memoria_activa.chars)
+	# print(memoria_activa.bools)
+	# print(memoria_activa.temp_int)
+	# print(memoria_activa.temp_float)
+	# print(memoria_activa.temp_char)
+	# print(memoria_activa.temp_bool)
+	# print("------------------------------------------------------------------------------------")
 
 	while cuadruplos[cont_cuadruplos][0] != 'ENDPROGRAM':
 		cuadruplo = cuadruplos[cont_cuadruplos]
-		print(cuadruplo)
+		#print(cuadruplo)
+
 		if cuadruplo[0] == 'SUMA':
-			op1 = cuadruplo[1]
+			op1 = cuadruplo[1] 
 			op2 = cuadruplo[2]
-
-			if op1 >= 20000 and op1 < 30000:
-				valorOp1 = memoria_global.getValorDeDireccion(op1, constantes)
-			else:
-				valorOp1 = memoria_activa.getValorDeDireccion(op1, constantes)
-
-			if op2 >= 20000 and op2 < 30000:
-				valorOp2 = memoria_global.getValorDeDireccion(op2, constantes)
-			else:
-				valorOp2 = memoria_activa.getValorDeDireccion(op2, constantes)
-
-			print(valorOp2)
-			result = valorOp1 + valorOp2
 			guarda =cuadruplo[3]
+			
+			op1 = str(op1)
+			if op1.find('(') != -1: # si encuentra ( ) tiene una direccion relativa
+				
+				otro = op1.replace("(", "") # quita los ( ) de la direccion relativa
+				otro2 = otro.replace(")", "")
+				op1 = int(otro2) # convertir a entera la direccion
+				
+				if op1 >= 20000 and op1 < 30000:
+					valorOp1 = memoria_global.getValorDeDireccion(op1, constantes)
+				else:
+					valorOp1 = memoria_activa.getValorDeDireccion(op1, constantes)
+				if valorOp1 < 0:
+					print("Error: indice de arreglo no puede ser negativo.")
+					exit()
+				sumale = valorOp1 + op2 # suma el valor de la direccion relativa a la direccion base
+				sumale = int(sumale) # genera una nueva direccion para accesar el arreglo
+				
+				if sumale >= 20000 and sumale < 30000:
+					valorOp2 = memoria_global.getValorDeDireccion(sumale, constantes)
+				else:
+					valorOp2 = memoria_activa.getValorDeDireccion(sumale, constantes)
+				
+				result = valorOp2 # obtiene el valor de la direccion del arreglo
+
+
+			else:
+				# es una suma normal
+				op1 = int(op1)
+
+				if op1 >= 20000 and op1 < 30000:
+					valorOp1 = memoria_global.getValorDeDireccion(op1, constantes)
+				else:
+					valorOp1 = memoria_activa.getValorDeDireccion(op1, constantes)
+
+				if op2 >= 20000 and op2 < 30000:
+					valorOp2 = memoria_global.getValorDeDireccion(op2, constantes)
+				else:
+					valorOp2 = memoria_activa.getValorDeDireccion(op2, constantes)
+				
+				result = valorOp1 + valorOp2
 
 			memoria_activa.setValorDeDireccion(guarda, result)
 
@@ -300,7 +329,7 @@ def ejecutaMaquina(directorio, cuadruplos, constantes):
 					if op1 >= 20000 and op1 < 30000:
 						valorOp1 = memoria_global.getValorDeDireccion(op1, constantes)
 					else:
-						print(op1)
+						#print(op1)
 						valorOp1 = memoria_activa.getValorDeDireccion(op1, constantes)
 				
 				valorAgregado = cuadruplo[4]
@@ -347,7 +376,8 @@ def ejecutaMaquina(directorio, cuadruplos, constantes):
 		elif cuadruplo[0] == 'ERA':
 			nombre = cuadruplo[1].lower()
 			memoria = directorio[nombre_programa]['Funciones'][nombre]['Memoria']
-			memoria_nueva = Memoria(nombre, memoria)
+			temps = directorio[nombre_programa]['Temporales']['Memoria']
+			memoria_nueva = Memoria(nombre, memoria, temps)
 
 		elif cuadruplo[0] == 'GOSUB':
 			saltos.append(cont_cuadruplos)
@@ -370,15 +400,15 @@ def ejecutaMaquina(directorio, cuadruplos, constantes):
 		elif cuadruplo[0] == 'RET':
 			memoria_activa = stack_memoria.pop()
 			cont_cuadruplos = saltos.pop()
-			print(memoria_activa.name)
-			print(memoria_activa.ints)
-			print(memoria_activa.floats)
-			print(memoria_activa.chars)
-			print(memoria_activa.bools)
-			print(memoria_activa.temp_int)
-			print(memoria_activa.temp_float)
-			print(memoria_activa.temp_char)
-			print(memoria_activa.temp_bool)
+			# print(memoria_activa.name)
+			# print(memoria_activa.ints)
+			# print(memoria_activa.floats)
+			# print(memoria_activa.chars)
+			# print(memoria_activa.bools)
+			# print(memoria_activa.temp_int)
+			# print(memoria_activa.temp_float)
+			# print(memoria_activa.temp_char)
+			# print(memoria_activa.temp_bool)
 
 		elif cuadruplo[0] == 'PARAM':
 			op1 = cuadruplo[1]
